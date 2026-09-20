eb7881e240214d57e54b15d194ad504149c4eea2
Domz0 <105143040+Domz0@users.noreply.github.com>
2026-09-20 16:54:15 -0300

    same validation for new mail/pass than registrar

--- FRONTEND/cuenta/mi-cuenta/mi-cuenta.js
+++ FRONTEND/cuenta/mi-cuenta/mi-cuenta.js
@@ -46,3 +46,54 @@ cancelarEdicion.addEventListener("click", () => {
   formEditar.style.display = "none";
   editarDatos.style.display = "block";
 });
+
+//validacion datos nuevos
+const nuevoCorreo = $("#nuevo_correo");
+const nuevaPassword = $("#nueva_password");
+
+const mensajeCorreo = $("#mensaje_correo");
+const mensajePassword = $("#mensaje_password");
+
+const guardarDatos = $("#guardar_datos");
+
+const regexCorreo = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+const regexMayuscula = /[A-Z]/;
+
+guardarDatos.addEventListener("click", () => {
+  let datosValidos = true;
+
+  // Nuevo correo
+  if (nuevoCorreo.value.trim() === "") {
+    mensajeCorreo.textContent = "Ingresa un correo";
+    datosValidos = false;
+  } else if (!regexCorreo.test(nuevoCorreo.value.trim())) {
+    mensajeCorreo.textContent = "Correo no válido";
+    datosValidos = false;
+  } else {
+    mensajeCorreo.textContent = "";
+  }
+
+  // Nueva contraseña
+  if (nuevaPassword.value.trim() === "") {
+    mensajePassword.textContent = "Ingresa una contraseña";
+    datosValidos = false;
+  } else if (nuevaPassword.value.length < 8) {
+    mensajePassword.textContent = "Debe tener mínimo 8 caracteres";
+    datosValidos = false;
+  } else if (!regexMayuscula.test(nuevaPassword.value)) {
+    mensajePassword.textContent = "Debe contener al menos una mayúscula";
+    datosValidos = false;
+  } else {
+    mensajePassword.textContent = "";
+  }
+
+  if (datosValidos) {
+    localStorage.setItem("correo", nuevoCorreo.value.trim());
+    localStorage.setItem("password", nuevaPassword.value);
+
+    correo.textContent = nuevoCorreo.value.trim();
+
+    formEditar.style.display = "none";
+    editarDatos.style.display = "block";
+  }
+});
